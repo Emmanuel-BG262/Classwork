066aecbe04217ed06c61c9e4ade6f5adb6b28e72
Emma-KXA <159710400+Emmanuel-BG262@users.noreply.github.com>
2024-02-28 20:55:41 +0530

    Add files via upload

---
+++ Tutorial.c
@@ -0,0 +1,50 @@
+//Tutorial No. 2
+//Emmanuel B G
+//Roll No.:30
+
+
+//The Square of the input no.
+#include <stdio.h>
+int main (){
+    int num ;
+    printf("Enter a integer:"); scanf("%d",&num);
+    printf("Square of %d is %d.",num ,(num*num));
+    return 0;
+}
+
+//To find theavg of any 3 no.s
+
+/*#include <stdio.h>
+int main (){
+    float no1,no2,no3 ;
+    printf("Enter any three integers(a b c):"); scanf("%f%f%f",&no1,&no2,&no3);
+    printf("Average of %.1f %.1f and %.1f is:%.1f",no1,n02,no3 ,(no1+no2+no3)/3);
+    return 0;
+}*/
+
+//Arithemetic Operations on any two no.s
+
+/*#include <stdio.h>
+int main(){
+    int no1,no2;
+    printf("Enter any two integers(a b):"); scanf("%d%d",&no1,&no2);
+    printf("Sum of %d + %d :%d",no1,no2,(no1+no2));
+    printf("\nDifference betweenw %d and %d :%d",no1,no2,(no1-no2));
+    printf("\nProduct of %d and %d :%d",no1,no2,(no1*no2));
+    printf("\nDivision of %d and %d :%.1f",no1,no2,(no1/no2));
+    printf("\nModulus %d %% %d :%.3f",no1,no2,(no1%no2));
+    return 0;
+}*/
+
+//Printing personal details
+
+/*#include <stdio.h>
+int main(){
+    char Name[25],Adrs[20],city[15],ph[10];
+    printf("Enter your name:"); gets(Name);
+    printf("Enter your Address:");  gets(Adrs);
+    printf("Enter city:"); gets(city);
+    printf("Enter Ph.No:"); gets(ph);
+    printf("\nName:%s\nAddress:%s\nCity:%s\nPhone No:%s",Name,Adrs,city,ph);
+    return 0;
+}*/
