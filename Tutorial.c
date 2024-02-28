3c9b43ac7e078bce299a2f248ddd84b6623b1158
Emma-KXA <159710400+Emmanuel-BG262@users.noreply.github.com>
2024-02-28 21:51:19 +0530

    Update Tutorial.c

--- Tutorial.c
+++ Tutorial.c
@@ -7,7 +7,8 @@
 #include <stdio.h>
 int main (){
     int num ;
-    printf("Enter a integer:"); scanf("%d",&num);
+    printf("Enter a integer:");
+    scanf("%d",&num);
     printf("Square of %d is %d.",num ,(num*num));
     return 0;
 }
@@ -17,7 +18,8 @@ int main (){
 /*#include <stdio.h>
 int main (){
     float no1,no2,no3 ;
-    printf("Enter any three integers(a b c):"); scanf("%f%f%f",&no1,&no2,&no3);
+    printf("Enter any three integers(a b c):");
+    scanf("%f%f%f",&no1,&no2,&no3);
     printf("Average of %.1f %.1f and %.1f is:%.1f",no1,n02,no3 ,(no1+no2+no3)/3);
     return 0;
 }*/
@@ -27,7 +29,8 @@ int main (){
 /*#include <stdio.h>
 int main(){
     int no1,no2;
-    printf("Enter any two integers(a b):"); scanf("%d%d",&no1,&no2);
+    printf("Enter any two integers(a b):");
+    scanf("%d%d",&no1,&no2);
     printf("Sum of %d + %d :%d",no1,no2,(no1+no2));
     printf("\nDifference betweenw %d and %d :%d",no1,no2,(no1-no2));
     printf("\nProduct of %d and %d :%d",no1,no2,(no1*no2));
@@ -41,10 +44,14 @@ int main(){
 /*#include <stdio.h>
 int main(){
     char Name[25],Adrs[20],city[15],ph[10];
-    printf("Enter your name:"); gets(Name);
-    printf("Enter your Address:");  gets(Adrs);
-    printf("Enter city:"); gets(city);
-    printf("Enter Ph.No:"); gets(ph);
+    printf("Enter your name:");
+    gets(Name);
+    printf("Enter your Address:"); 
+    gets(Adrs);
+    printf("Enter city:");
+    gets(city);
+    printf("Enter Ph.No:");
+    gets(ph);
     printf("\nName:%s\nAddress:%s\nCity:%s\nPhone No:%s",Name,Adrs,city,ph);
     return 0;
 }*/
